514477fe501c7040776d0f92f7675618e5f0f90d
Rob Maclean <40338730@live.napier.ac.uk>
2019-03-07 20:57:42 +0000

    Add Report 6 method

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -89,6 +89,64 @@ public class App {
     
 
 
+    // REPORT 6: produce a report listing the top N populated countries in a region where N is provided by the user
+    public static String getReport6()
+    {
+        // Create user input variable
+        int userInput = 7;
+
+        String results = "";
+        try
+        {
+
+            // SELECT STATEMENT
+            String strSelect = "SELECT Name, Region, Population " +
+                    " FROM country " +
+                    " WHERE region = 'Middle East' " +
+                    " ORDER BY Population DESC " +
+                    " LIMIT " + userInput;
+
+
+            Statement stmt = con.createStatement();
+            // Statement conn = con.createStatement();
+
+            // Execute SQL statement
+            stmt.executeQuery(strSelect);
+
+            ResultSet rset = stmt.executeQuery(strSelect);
+
+            // Check one is returned
+            System.out.println( "\n" + "*** Top " + userInput + " populated countries in the Middle East: ***" );
+            System.out.println( "Country:" + "\t" + "Population:" );
+
+            while (rset.next())
+            {
+                world wd = new world();
+
+                // Fields to be shown
+                // wd.Country = rset.getString("Country");
+                wd.Name = rset.getString("Name");
+                wd.Population = rset.getString("Population");
+
+
+
+                System.out.println( wd.Name + "\t" + wd.Population );
+                String newRES =   wd.Name + "\t" + wd.Population +"\n";
+
+                // Build Results
+                results = results + newRES;
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get world details");
+            return null;
+        }
+        return results;
+    }
+
+
     // REPORT 8: produce a report listing all the cities in a continent organised by largest population to smallest
     public static String getReport8()
     {
