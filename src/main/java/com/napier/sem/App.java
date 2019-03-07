a1ecd9c1b15b7211a7a1e31d7f417839fa35c03d
Rob Maclean <40338730@live.napier.ac.uk>
2019-03-07 20:52:20 +0000

    Add Report 16 method

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -88,6 +88,68 @@ public class App {
 
     
 
+
+
+    // REPORT 16:  produce a report listing the top N populated cities in a district where N is provided by the user
+    public static String getReport16()
+    {
+        // Create user input variable
+        int userInput = 5;
+
+        String results = "";
+        try
+        {
+
+            // SELECT STATEMENT
+            String strSelect = "SELECT city.Name, city.Population " +
+                    " FROM city " +
+                    " JOIN country ON city.CountryCode=country.Code " +
+                    " WHERE district = 'Tabasco' " +
+                    " ORDER BY city.Population DESC " +
+                    " LIMIT " + userInput;
+
+
+            Statement stmt = con.createStatement();
+            // Statement conn = con.createStatement();
+
+            // Execute SQL statement
+            stmt.executeQuery(strSelect);
+
+            ResultSet rset = stmt.executeQuery(strSelect);
+
+            // Check one is returned
+            System.out.println( "\n" + "*** The top " +  userInput + " most populated cities in Tabasco, Mexico: ***" );
+            System.out.println( "City:" + "\t" + "Population:" );
+
+            while (rset.next())
+            {
+                world wd = new world();
+
+                // Fields to be shown
+                // wd.Country = rset.getString("Country");
+                wd.Name = rset.getString("Name");
+                wd.Population = rset.getString("Population");
+
+
+
+                System.out.println( wd.Name + "\t" + wd.Population );
+                String newRES =   wd.Name + "\t" + wd.Population +"\n";
+
+                // Build Results
+                results = results + newRES;
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get world details");
+            return null;
+        }
+        return results;
+    }
+
+
+
     // REPORT 18: produce a report listing all the capital cities in a continent organised by largest population to smallest
     public static String getReport18()
     {
