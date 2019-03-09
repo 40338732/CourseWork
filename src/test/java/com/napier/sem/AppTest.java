b80f7ad42793ad8c05fab1d9e4e04d9ba2e3aed6
40338732 <40338732@napier.live.ac.uk>
2019-03-09 13:22:02 +0000

    Starting Lab 7 - first two tests completed wth expected results at stage "Employees is Empty" working as lab dictates
    completed lab 7

--- src/test/java/com/napier/sem/AppTest.java
+++ src/test/java/com/napier/sem/AppTest.java
@@ -52,4 +52,29 @@ public class AppTest
         app.printSalaries(employess);
     }
 
+    // Employees Contains null
+    @Test
+    void printSalariesTestContainsNull()
+    {
+        ArrayList<Employee> employess = new ArrayList<Employee>();
+        employess.add(null);
+        app.printSalaries(employess);
+    }
+
+    // Employee Contains All Non-null
+    @Test
+    void printSalaries()
+    {
+        ArrayList<Employee> employees = new ArrayList<Employee>();
+        Employee emp = new Employee();
+        emp.emp_no = 1;
+        emp.first_name = "Kevin";
+        emp.last_name = "Chalmers";
+        emp.title = "Engineer";
+        emp.salary = 55000;
+        employees.add(emp);
+        app.printSalaries(employees);
+    }
+
+
 }
