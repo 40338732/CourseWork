63037982dd26771ea1188effd65b5757de39213d
Rob <40338730@live.napier.ac.uk>
2019-03-19 16:50:34 +0000

    added first test and delted submodule

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -26,6 +26,14 @@ public class AppIntegrationTest
         String[] s = App.getReport2().split("\t", 2);
         //System.out.println("Results" + s[0]);
         assertEquals(s[0], "Russian Federation");
-//
+    }
+
+    @Test
+    void testGetReport4()
+    {
+        // REPORT 4: produce a report listing the top N populated countries in the World where N is provided by the user
+        String[] s = App.getReport4().split("\t", 2);
+        //System.out.println("Results" + s[0]);
+        assertEquals(s[0], "Russian Federation");
     }
 }
