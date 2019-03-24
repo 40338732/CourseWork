7dae4c4007da472251d60b58740db43df8a05697
Rob Maclean <40338730@live.napier.ac.uk>
2019-03-24 11:59:38 +0000

    Added integration tests for even reports

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -24,8 +24,97 @@ public class AppIntegrationTest
     {
         // REPORT 2: list all the countries in a continent organised by largest population to smallest.
         String[] s = App.getReport2().split("\t", 2);
-        //System.out.println("Results" + s[0]);
         assertEquals(s[0], "Russian Federation");
     }
-    
+
+    @Test
+    void testGetReport4()
+    {
+        // REPORT 4: produce a report listing the top N populated countries in the World where N is provided by the user
+        String[] s = App.getReport4(2).split("\t", 4);
+        assertEquals(s[0], "China");
+    }
+
+
+    @Test
+    void testGetReport6()
+    {
+        // REPORT 6: produce a report listing the top N populated countries in a region where N is provided by the user
+        String[] s = App.getReport6().split("\t", 4);
+        assertEquals(s[0], "Turkey");
+    }
+
+    @Test
+    void testGetReport8()
+    {
+        // REPORT 8: produce a report listing all the cities in a continent organised by largest population to smallest
+        String[] s = App.getReport8().split("\t", 2);
+        assertEquals(s[0], "Cairo");
+    }
+
+    @Test
+    void testGetReport10()
+    {
+        // REPORT 10: produce a report listing all the cities in a country organised by largest population to smallest
+        String[] s = App.getReport10().split("\t", 2);
+        assertEquals(s[0], "London");
+    }
+
+    @Test
+    void testGetReport12()
+    {
+        // REPORT 12: produce a report listing the top N populated cities in the World where N is provided by the user
+        String[] s = App.getReport12().split("\t", 2);
+        assertEquals(s[0], "Mumbai (Bombay)");
+    }
+
+    @Test
+    void testGetReport14()
+    {
+        // REPORT 14:  produce a report listing the top N populated cities in a region where N is provided by the user
+        String[] s = App.getReport14().split("\t", 2);
+        assertEquals(s[0], "São Paulo");
+    }
+
+    @Test
+    void testGetReport16()
+    {
+        // REPORT 16:  produce a report listing the top N populated cities in a district where N is provided by the user
+        String[] s = App.getReport16().split("\t", 2);
+        assertEquals(s[0], "Centro (Villahermosa)");
+    }
+
+    @Test
+    void testGetReport18()
+    {
+        // REPORT 18: produce a report listing all the capital cities in a continent organised by largest population to smallest
+        String[] s = App.getReport18().split("\t", 2);
+        assertEquals(s[0], "Moscow");
+    }
+
+    @Test
+    void testGetReport20()
+    {
+        // REPORT 20: produce a report listing the top N populated capital cities in the World where N is provided by the user
+        String[] s = App.getReport20().split("\t", 2);
+        assertEquals(s[0], "Seoul");
+    }
+
+    @Test
+    void testGetReport22()
+    {
+        // REPORT 22: produce a report listing the top N populated capital cities in a region where N is provided by the user
+        String[] s = App.getReport22().split("\t", 2);
+        assertEquals(s[0], "La Habana");
+    }
+
+    @Test
+    void testGetReport24()
+    {
+        // REPORT 22: produce a report listing the top N populated capital cities in a region where N is provided by the user
+        String[] s = App.getReport24().split("\t", 2);
+        //System.out.println("Result: " + s[0]);
+        assertEquals(s[0], "Southern and Central Asia");
+    }
+
 }
